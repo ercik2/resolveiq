b43fe486faab916f2998b2802cac10dad1d1d4b1
root <root@Main1.helpdesk.local>
2026-05-01 01:26:06 +0000

    Update API URL for k3s

--- helpdesk-frontend/src/servicios/api.js
+++ helpdesk-frontend/src/servicios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.56.101:3000'
+    baseURL: 'http://127.0.0.1:3000'
 });
 
 api.interceptors.request.use((config) => {
