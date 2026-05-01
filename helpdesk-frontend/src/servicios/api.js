077a217406fadb26f2a7277e89f49b76678130e9
root <root@Main1.helpdesk.local>
2026-05-01 02:03:52 +0000

    otra ve

--- helpdesk-frontend/src/servicios/api.js
+++ helpdesk-frontend/src/servicios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8080/api'
+    baseURL: 'http://127.0.0.1:3000'
 });
 
 api.interceptors.request.use((config) => {
